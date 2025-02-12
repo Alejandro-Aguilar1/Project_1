0115bbf0a2acc89c9c1ce962d632d611e995f49c
Alejandro Aguilar <aguilar.alex2806@gmail.com>
2025-02-12 13:04:01 -0600

    Changed the name of the input variable to fit the problem statement.

--- src/main/kotlin/client.kt
+++ src/main/kotlin/client.kt
@@ -3,15 +3,13 @@ import java.util.Scanner
 
 fun main(){
     val scanner = Scanner(System.`in`)
-    println("Enter input string (end with '.'")
-    val input = scanner.nextLine()
+    println("Enter input string, a sequence of T's, A's, and M's (end with '#')")
+    val tamTabClient = scanner.nextLine()
 
     Socket("localhost", 9999).use { socket ->
         val writer = socket.outputStream.bufferedWriter()
-        writer.write("$input\n")
+        writer.write("$tamTabClient\n")
         writer.flush()
-        // Make sure to not close the writer here
-
         socket.getInputStream().bufferedReader().use { reader ->
             println("Input contains sequence: ${reader.readLine()}")
         }
